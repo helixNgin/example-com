fed98d2fef18243747c013a633c8209857a6e85f
Daniel Lee <daniel@DanieldeMacBook-Pro.local>
2022-06-15 13:36:33 +0800

    TEST:pseudo version

---
+++ hello2/go.mod
@@ -0,0 +1,3 @@
+module github.com/helixNgin/example-com/v2/hello2
+
+go 1.18
---
+++ hello2/hello2.go
@@ -0,0 +1,9 @@
+/**
+    * @Description:
+    * @Author: daniel(2022/6/15)
+**/
+package hello2
+
+func Hello2(str string) string {
+	return "Hello2, " + str
+}
